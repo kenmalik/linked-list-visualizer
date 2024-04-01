c2ee79a5e4f7345dffd131769f5b745a938bdb97
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2024-03-31 20:27:23 -0700

    Separate drawing to window to separate function

--- src/main.cpp
+++ src/main.cpp
@@ -11,24 +11,28 @@
 #include <SFML/Window/Event.hpp>
 #include <SFML/Window/VideoMode.hpp>
 #include <iostream>
+#include <vector>
+
+void LoadFont(sf::Font &font);
+void DrawToScreen(sf::RenderWindow &window,
+                  const std::vector<sf::Drawable *> &drawables);
 
 int main() {
   constexpr size_t kScreenWidth = 1280;
   constexpr size_t kScreenHeight = 720;
+  sf::RenderWindow window({kScreenWidth, kScreenHeight, 32}, "Window");
 
   sf::Font font;
-  if (!font.loadFromFile("../assets/Ubuntu-R.ttf")) {
-    std::cerr << "Cannot load font";
-    exit(1);
-  }
-
-  sf::RenderWindow window({kScreenWidth, kScreenHeight, 32}, "Window");
+  LoadFont(font);
 
   NodeShape<int> node({300 * 1.5, 100 * 1.5}, font);
   node.setData({10, font});
   node.setFillColor(sf::Color::White, sf::Color::Green,
                     sf::Color(224, 120, 22));
   node.setLeftPosition({50, 150});
+
+  std::vector<sf::Drawable *> drawables;
+  drawables.push_back(&node);
 
   while (window.isOpen()) {
     sf::Event event;
@@ -37,13 +41,25 @@ int main() {
         window.close();
       }
     }
-
-    window.clear();
-
-    window.draw(node);
-
-    window.display();
+    DrawToScreen(window, drawables);
   }
 
   return 0;
+}
+
+void LoadFont(sf::Font &font) {
+  const std::string font_path = "../assets/Ubuntu-R.ttf";
+  if (!font.loadFromFile(font_path)) {
+    std::cerr << "Cannot load font at " << font_path;
+    exit(1);
+  }
+}
+
+void DrawToScreen(sf::RenderWindow &window,
+                  const std::vector<sf::Drawable *> &drawables) {
+  window.clear();
+  for (const auto &drawable : drawables) {
+    window.draw(*drawable);
+  }
+  window.display();
 }
